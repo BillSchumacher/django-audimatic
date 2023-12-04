9d48935983085f8fed0fd43b3f00f57ef8472945
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-12-04 16:03:19 -0600

    Maybe fix test.

--- django_audimatic/acceptance/steps/then.py
+++ django_audimatic/acceptance/steps/then.py
@@ -5,5 +5,7 @@ from behave import then
 @then('the "{model_name}" model should have an audit trail')
 def step_impl(context, model_name):
     """Check that the model has an audit trail."""
-    model = context.test.models[model_name]
-    context.test.assertTrue(hasattr(model, "audit_trail"))
+    model = context.models[model_name]
+    print(dir(model))
+    print(dir(model._meta))
+    context.test.assertTrue(hasattr(model, "get_audit_trail"))
